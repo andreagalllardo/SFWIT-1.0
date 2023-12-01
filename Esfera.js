084ecd452723120157107bdf37a697ef5c15d439
Andrea Gallardo Gonzalez <151090533+andreagalllardo@users.noreply.github.com>
2023-11-30 23:51:53 -0600

    Add files via upload

---
+++ Esfera.js
@@ -0,0 +1,7 @@
+// Calcular Volumen de Esfera con el Radio
+let radio= parseFloat(prompt('radio:'));
+let pi= 3.1416;
+volumen= 4/3 * pi * (radio*radio*radio);
+document.write(volumen + "metros cubicos")
+
+
